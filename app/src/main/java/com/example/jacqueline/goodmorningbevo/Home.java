75473ddc88b8a142b482a7859bd93aa5e02a67fa
jdeans289 <jdeans289@gmail.com>
2018-02-17 16:04:11 -0600

    Added new screens

--- app/src/main/java/com/example/jacqueline/goodmorningbevo/Home.java
+++ app/src/main/java/com/example/jacqueline/goodmorningbevo/Home.java
@@ -1,13 +1,39 @@
 package com.example.jacqueline.goodmorningbevo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    Button alarmButton, taskButton, otherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        alarmButton = (Button)findViewById(R.id.button4);
+        taskButton = (Button)findViewById(R.id.button6);
+        otherButton = (Button)findViewById(R.id.button7);
+
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, AlarmEditor.class));
+            }
+        });
+
+        taskButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this, TaskEditor.class));
+            }
+        });
     }
 }
